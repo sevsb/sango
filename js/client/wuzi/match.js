8980e6841e4a86e2edeca124d5b19236f5e472f5
樱凌准 <SakuraSinojun@gmail.com>
2017-07-04 15:44:49 +0800

    use websocket.
    Change-Id: I6a856bdea7dde1e0d1628fe9418f6d5a0a91d2dc

--- js/client/wuzi/match.js
+++ js/client/wuzi/match.js
@@ -1,13 +1,46 @@
-function refresh() {
-    var matchid = $("#matchconfig").attr("match");
-    var lastplace = $("#matchconfig").attr("lastplaceid");
-    if (lastplace == -1) {
-        return;
-    }
-    __ajax("client.wuzi.refresh", {matchid: matchid, placeid: lastplace}, true, function(data) {
-        console.debug("no new place.");
-    });
+var ws = null;
+
+var walkto = function(dir) {
+    console.debug("walk to " + dir);
+    var obj = {};
+    obj.op = "walk";
+    obj.to = dir;
+    send(obj);
 }
+
+var send = function(obj) {
+    var text = JSON.stringify(obj);
+    console.debug("send: " + text);
+    ws.send(text);
+}
+
+var send_op = function(op) {
+    var text = '{"op": "' + op + '"}';
+    console.debug("send: " + text);
+    ws.send(text);
+}
+
+var login = function() {
+    var me = $("#matchconfig").attr("me");
+    var matchid = $("#matchconfig").attr("match");
+
+    var obj = {};
+    obj.op = "login";
+    obj.player = me;
+    send(obj);
+
+    obj.op = "match";
+    obj.match = matchid;
+    send(obj);
+}
+
+var place_piece = function(place) {
+    var obj = {};
+    obj.op = "place";
+    obj.place = place;
+    send(obj);
+}
+
 
 $(document).ready(function() {
     console.debug("docuemnt.ready.");
@@ -27,15 +60,21 @@ $(document).ready(function() {
         console.debug(place);
 
         var matchid = $("#matchconfig").attr("match");
-        __ajax_and_reload("client.wuzi.place", {matchid: matchid, place: place});
+        // __ajax_and_reload("client.wuzi.place", {matchid: matchid, place: place});
+
+        place_piece(place);
     });
 
     var gridwidth = 0;
     $(".board-node").each(function() {
         if (gridwidth == 0) {
             gridwidth = $(this)[0].offsetWidth;
+            if (gridwidth > 50) {
+                gridwidth = 50;
+            }
             console.debug(gridwidth);
         }
+        $(this).css("width", gridwidth + "px");
         $(this).css("height", gridwidth + "px");
 
         w1 = gridwidth * 18 / 25;
@@ -47,7 +86,43 @@ $(document).ready(function() {
         $(this).children(".piece").css("top", t + "px");
     });
 
-    // setInterval("refresh()", 2000);
-});
 
+    ws = new WebSocket('ws://114.215.82.75:19504');
+    ws.onopen = function(evt) {
+        // msg.innerHTML = ws.readyState;
+        console.debug(ws);
+        login();
+    };
+
+    ws.onclose = function(evt) {
+        console.debug(evt);
+    };
+
+    ws.onmessage = function(evt) {
+        var obj = eval('(' + evt.data + ')');
+        console.debug(obj);
+        if (obj.op == "tip") {
+            var ret = obj.ret;
+            if (typeof(obj.reason) == "string") {
+                alert(obj.reason);
+            }
+        } else if (obj.op == "place") {
+            var place = obj.place;
+            var player = obj.player;
+            var pid1 = $(".player1").attr("pid");
+            var pid2 = $(".player2").attr("pid");
+            var clz = "white-piece";
+            if (player == pid1) {
+                clz = "black-piece";
+            }
+            $("div[place='" +place + "']").addClass(clz);
+            // document.location.reload();
+        }
+    };
+
+    ws.onerror = function(evt, e) {
+        console.error(evt)
+        // console.error(ws)
+    };
+});
 
